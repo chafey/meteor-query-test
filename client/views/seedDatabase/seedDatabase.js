0a0bd22bc78f6814ae0be71e1976a210396635b3
Abigail Watson <awatson1978@gmail.com>
2015-01-05 14:57:26 -0500

    moving to workflow/multi-device oriented mvc model

--- client/views/seedDatabase/seedDatabase.js
+++
@@ -1,8 +0,0 @@
-Template.seedDatabase.events({
-    'click #seed': function () {
-        Meteor.call('seedDatabase');
-    },
-    'click #clear': function () {
-        Meteor.call('clearDatabase');
-    }
-});
